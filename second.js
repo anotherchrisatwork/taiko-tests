0ccb8678f66a152f639b2b7957007f09b596faa2
chris <chris.sterritt@gd-ms.com>
2019-11-04 13:51:30 -0500

    Now HEADLESS env var respected, added two-todo-add and todo-delete tests.

--- second.js
+++ second.js
@@ -9,7 +9,7 @@ const {
   click,
 } = require('taiko')
 const { expect } = require('chai')
-const headless = false
+const headless = process.env.HEADLESS === 'true'
 
 // -------------------------------------------------------------
 const FindTextOnPage = async (textToFindOnPage) => {
@@ -17,8 +17,14 @@ const FindTextOnPage = async (textToFindOnPage) => {
   expect(await txt.exists()).to.be.true
 }
 
+const pause = async () => {
+  return new Promise((resolve) => {
+    setTimeout(() => resolve(true), 5000)
+  })
+}
+
 // -------------------------------------------------------------
-describe('Getting Started with Mocha and Taiko', () => {
+describe('Adding todos - one', async () => {
   before(async () => {
     await openBrowser({ headless: headless })
     await goto('http://localhost:4000/api/clean-database')
@@ -27,7 +33,7 @@ describe('Getting Started with Mocha and Taiko', () => {
   describe('Add one todo', () => {
     it('after DB reset, todo list is empty', async () => {
       await goto('http://localhost:3000')
-      FindTextOnPage('0 / 0')
+      await FindTextOnPage('0 / 0')
     })
 
     it('create a new todo', async () => {
@@ -36,7 +42,7 @@ describe('Getting Started with Mocha and Taiko', () => {
     })
 
     it('new todo text found on page', async () => {
-      FindTextOnPage('Foo')
+      await FindTextOnPage('Foo')
     })
 
     it('text entry is now empty', async () => {
@@ -44,7 +50,82 @@ describe('Getting Started with Mocha and Taiko', () => {
     })
 
     it('list has one element yet to be done', async () => {
-      FindTextOnPage('1 / 1')
+      await FindTextOnPage('1 / 1')
+    })
+  })
+
+  after(async () => {
+    await closeBrowser()
+  })
+})
+
+// -------------------------------------------------------------
+describe('Adding todos - two', async () => {
+  before(async () => {
+    await openBrowser({ headless: headless })
+    await goto('http://localhost:4000/api/clean-database')
+  })
+
+  describe('Add two todos', () => {
+    it('after DB reset, todo list is empty', async () => {
+      await goto('http://localhost:3000')
+      await FindTextOnPage('0 / 0')
+    })
+
+    it('create two new todos', async () => {
+      await write('Foo', into(textBox()))
+      await click('Save')
+      await write('Bar', into(textBox()))
+      await click('Save')
+    })
+
+    it('new todos texts found on page', async () => {
+      await FindTextOnPage('Foo')
+      await FindTextOnPage('Bar')
+    })
+
+    it('text entry is now empty', async () => {
+      expect(await textBox().value()).to.eql('')
+    })
+
+    it('list has two elements yet to be done', async () => {
+      await FindTextOnPage('2 / 2')
+    })
+  })
+
+  after(async () => {
+    await closeBrowser()
+  })
+})
+
+// -------------------------------------------------------------
+describe('Deleting a todo', async () => {
+  before(async () => {
+    await openBrowser({ headless: headless })
+    await goto('http://localhost:4000/api/clean-database')
+  })
+
+  describe('Add one todo', () => {
+    it('after DB reset, todo list is empty', async () => {
+      await goto('http://localhost:3000')
+      await FindTextOnPage('0 / 0')
+    })
+
+    it('create a new todo', async () => {
+      await write('Foo', into(textBox()))
+      await click('Save')
+    })
+
+    it('new todo text found on page', async () => {
+      await FindTextOnPage('Foo')
+    })
+
+    it('delete the todo', async () => {
+      await click('X')
+    })
+
+    it('list has no elements yet to be done', async () => {
+      await FindTextOnPage('0 / 0')
     })
   })
 
