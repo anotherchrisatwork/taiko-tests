0aaf884920516de09ed0d7f1497e386ecc7ef499
chris <chris.sterritt@gd-ms.com>
2019-11-04 11:05:56 -0500

    Using chai for expect, doing better test.

--- second.js
+++ second.js
@@ -8,20 +8,32 @@ const {
   textBox,
   click,
 } = require('taiko')
-const assert = require('assert')
+const { expect } = require('chai')
 const headless = false
 
 describe('Getting Started with Mocha and Taiko', () => {
   before(async () => {
     await openBrowser({ headless: headless })
+    await goto('http://localhost:4000/api/clean-database')
   })
 
   describe('Add one todo', () => {
-    it('Goto getgauge github page', async () => {
+    it('add the "Foo" todo, check that it shows up on the page', async () => {
       await goto('http://localhost:3000')
+      // after DB reset, todo list is empty
+      let txt = await text('0 / 0')
+      expect(await txt.exists()).to.be.true
+      // create a new todo
       await write('Foo', into(textBox()))
       await click('Save')
-      assert.ok(await text('Foo').exists())
+      // find its text on the page
+      txt = await text('Foo')
+      expect(await txt.exists()).to.be.true
+      // text entry is now empty
+      expect(await textBox().value()).to.eql('')
+      // list has one element yet to be done
+      txt = await text('1 / 1')
+      expect(await txt.exists()).to.be.true
     })
   })
 
